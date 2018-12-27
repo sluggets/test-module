e3b7a7e5d8cd8564c27c98daff243d1e5908a03e
timothy <tim.longoria@gmail.com>
2018-12-27 10:33:06 -0600

    Adds test widget

--- views/default/index.php
+++ views/default/index.php
@@ -1,1 +1,16 @@
+<?php
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\bootstrap\Alert;
+?>
 <?= "MyModule: Hello World!"; ?>
+<?php $widget = Alert::begin([
+        'options' => [
+                'class' => 'alert-warning',
+        ],
+]);
+?>
+<p>This is an alert example</p>
+<?php
+$widget->end();
+?>
